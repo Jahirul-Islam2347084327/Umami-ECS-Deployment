112d74b387971e3e256e5ecb659f4353447c4084
Jahirul Islam <jahirul.islam200518@gmail.com>
2026-07-04 15:28:09 +0100

    seperated the s3 and dynamo from infra code to allow for targetted deployment of modules

--- terraform/workflows/development/backend-infra/main.tf
+++ terraform/workflows/development/backend-infra/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "jahis-devops-directive-state-2026"
-    key = "tf-infra/terraform.tfstate"
+    key = "backend-infra/terraform.tfstate"
     region = "us-east-1" # change this to your desired region
     dynamodb_table = "terraform-state-locking"
     encrypt = true
@@ -18,44 +18,19 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "terraform_state" { 
-  bucket = "jahis-devops-directive-state-2026"
-  force_destroy = true
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "terraform-locks" {
-  name = "terraform-state-locking"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 module "vpc" {
-  source = "../../modules/network"
+  source = "../../../modules/network"
   az1 = var.az1
   az2 = var.az2
   region = var.region
 }
 
 module "ecr" {
-  source = "../../modules/ecr"
+  source = "../../../modules/ecr"
 }
 
 module "ecs" {
-  source = "../../modules/ecs"
+  source = "../../../modules/ecs"
   target-group-arn = module.alb.target-group-arn
   ecs-security-group-id = module.security.ecs-security
   database-url = module.rds.rds-url
@@ -65,7 +40,7 @@ module "ecs" {
 }
 
 module "alb" {
-  source = "../../modules/alb"
+  source = "../../../modules/alb"
   alb-security-group-id = module.security.alb-security
   certificate-arn = module.route53.acm-arn
   public-subnets-id = module.vpc.public-subnet-ids
@@ -73,30 +48,30 @@ module "alb" {
 }
 
 module "rds" {
-  source = "../../modules/rds"
+  source = "../../../modules/rds"
   private-subnet-ids = module.vpc.private-subnet-ids
   rds-sg-id = module.security.rds-security
 }
 
 module "security" {
-  source = "../../modules/security"
+  source = "../../../modules/security"
   vpc-id = module.vpc.vpc-id
 }
 
 module "route53" {
-  source = "../../modules/route53"
+  source = "../../../modules/route53"
   alb-dns-name = module.alb.alb-dns
   alb-zone-id = module.alb.alb-zone-id
   custom-url =  var.custom-url
 }
 
 module "waf" {
-  source = "../../modules/waf"
+  source = "../../../modules/waf"
   alb-arn = module.alb.alb-arn
 }
 
 module "codedeploy" {
-  source = "../../modules/codedeploy"
+  source = "../../../modules/codedeploy"
   target-blue-name = module.alb.target-blue-name
   target-green-name = module.alb.target-green-name
   alb-listener = module.alb.alb-listener
