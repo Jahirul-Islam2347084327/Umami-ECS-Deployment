b05536a9ddc2f43c9a4950ca74dfa4bdcad4d7c5
Jahirul Islam <jahirul.islam200518@gmail.com>
2026-07-09 16:42:24 +0100

    bug fixes

--- terraform/workflows/development/backend-infra/variables.tf
+++ terraform/workflows/development/backend-infra/variables.tf
@@ -13,7 +13,7 @@ variable "az2" {
 variable "custom-url" {
   type = string
   description = "a domain that you own"
-  default = "jahirulmadethisin2026.com"
+  default = "jahirulmadethisin2026.online"
 }
 
 variable "region" {
