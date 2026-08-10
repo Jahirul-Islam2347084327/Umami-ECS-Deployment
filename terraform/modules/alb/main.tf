77257c0719ce90d09aae85262a13a7f2a8bcbea9
Jahirul Islam <jahirul.islam200518@gmail.com>
2026-08-10 14:39:47 +0100

    added stickiness rule to listener

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -39,6 +39,7 @@ resource "aws_lb_listener" "https" {
     default_action {
       type = "forward"
       target_group_arn = aws_lb_target_group.blue.arn
+
     }
 
 tags = {
@@ -46,6 +47,44 @@ tags = {
 }
 lifecycle {
     ignore_changes = [default_action]
+  }
+}
+
+resource "aws_lb_listener_rule" "canary" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100
+
+  action {
+    type = "forward"
+
+    forward {
+      target_group {
+        arn    = aws_lb_target_group.blue.arn
+        weight = 100
+      }
+
+      target_group {
+        arn    = aws_lb_target_group.green.arn
+        weight = 0
+      }
+
+      stickiness {
+        enabled  = true
+        duration = 300
+      }
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      action
+    ]
   }
 }
 
